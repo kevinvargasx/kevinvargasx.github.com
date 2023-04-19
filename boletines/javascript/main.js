081b6e2c0ce36a270c9480bedac5f3e3d4d5c760
deadkevinx <kevarsa@gmail.com>
2023-04-19 16:14:16 -0500

    nuevo cambio

---
+++ boletines/javascript/main.js
@@ -0,0 +1,49 @@
+
+console.log("Hola mundo");
+
+// DOM
+/*  
+let links = document.querySelectorAll("a");
+
+links.forEach(function(link){
+  console.log(link);
+});
+*/
+/*
+let celdas = document.querySelectorAll("td");
+
+celdas.forEach(function(td){
+    td.addEventListener('click',function(){
+        console.log(this);
+    })
+})
+*/
+
+let links = document.querySelectorAll(".close");
+
+links.forEach(function (link) {
+
+    // Agregar un evento Click a cada uno de ellos-case sensitive
+    link.addEventListener("click", function (evento) {
+        evento.preventDefault();
+
+        let content = document.querySelector('.content');
+        // quitarle las clases de animación que ya tiene
+        content.classList.remove("animate__fadeInDown");
+        content.classList.remove("animate__animated");
+        // Agregar clases para animar su salidad fadeOutUp
+        content.classList.add("animate__fadeOutDown");
+        content.classList.add("animate__animated");
+        //tiempo
+        setTimeout(function () {
+            location.href = "/";
+        }, 600);
+        /* setInterval(function(){
+             location.href = "/";
+         },1000);
+         */
+
+        return false;
+    })
+});
+
